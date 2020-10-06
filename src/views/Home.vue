7e6df00ab09733e9481908d49ec2e8cde4736ed7
David Li <davidli012345@gmail.com>
2020-10-06 03:33:11 +0000

    deploy some change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
-<Navbar />
-<Counter />
 <div class="home">
+  <Navbar />
+  <Counter />
   <img alt="Vue logo" src="../assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </div>
