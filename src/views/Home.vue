c322661198b0a6819b914be078595ad5600547fc
David Li <davidli012345@gmail.com>
2020-10-06 03:31:07 +0000

    updating website

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <Navbar />
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+<Navbar />
+<Counter />
+<div class="home">
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+</div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import {
+  Options,
+  Vue
+} from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
+import Counter from "@/components/Counter.vue";
 @Options({
   components: {
     HelloWorld,
