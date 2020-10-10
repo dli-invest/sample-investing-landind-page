2f747684c3ae0e1b9d3ec00c23e820fc9f29ad49
David Li <davidli012345@gmail.com>
2020-10-10 16:56:11 +0000

    updating components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
 <TitleHeader />
-<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
@@ -8,13 +7,11 @@ import {
     Options,
     Vue
 } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import Navbar from "@/components/Navbar.vue";
 import Counter from "@/components/Counter.vue";
 import TitleHeader from "@/components/TitleHeader.vue"
 @Options({
     components: {
-        HelloWorld,
         Navbar,
         Counter,
         TitleHeader
