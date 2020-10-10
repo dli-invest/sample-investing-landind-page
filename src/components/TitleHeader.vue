1aab147fa5d4eb3fd0cf047a558fcc45071fbe53
David Li <davidli012345@gmail.com>
2020-10-10 04:08:35 +0000

    centering content

--- src/components/TitleHeader.vue
+++ src/components/TitleHeader.vue
@@ -1,45 +1,43 @@
 <template>
 <div>
-    <div class="p-grid">
-        <div class="p-col-6 p-md-6 p-lg-6">
-            <h2>App, Business & SaaS</h2>
-            <h3>
-                Landing Page Template
-            </h3>
-        </div>
-        <div class="p-col-6 p-md-6 p-lg-6">
-            <img alt="Vue logo" src="../assets/undraw_investment_xv9d.svg" class="thero__image" />
-        </div>
+  <div class="p-grid">
+    <div class="p-col-6 p-md-6 p-lg-6 p-jc-center">
+      <h2>App, Business & SaaS</h2>
+      <h3>Landing Page Template</h3>
     </div>
-    <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#0099ff" fill-opacity="1" d="M0,224L60,218.7C120,213,240,203,360,176C480,149,600,107,720,106.7C840,107,960,149,1080,138.7C1200,128,1320,64,1380,32L1440,0L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-        </svg>
+    <div class="p-col-6 p-md-6 p-lg-6 p-jc-center">
+      <img alt="Vue logo" src="../assets/undraw_investment_xv9d.svg" class="thero__image" />
     </div>
+  </div>
+  <div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#0099ff" fill-opacity="1" d="M0,224L60,218.7C120,213,240,203,360,176C480,149,600,107,720,106.7C840,107,960,149,1080,138.7C1200,128,1320,64,1380,32L1440,0L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+    </svg>
+  </div>
 </div>
 </template>
 
 <style lang="scss">
 .thero {
-    color: #FF00FF;
+  color: #ff00ff;
 
-    &__disclaimer {
-        color: #635963
-    }
+  &__disclaimer {
+    color: #635963;
+  }
 
-    &__image {
-        max-width: 50%;
-        min-width: 400px;
-        display: inline-block;
-    }
+  &__image {
+    max-width: 50%;
+    min-width: 400px;
+    display: inline-block;
+  }
 }
 </style>
 
 <script lang="ts">
-import Card from "primevue/card"
+import Card from "primevue/card";
 export default {
-    components: {
-        Card
-    }
-}
+  components: {
+    Card,
+  },
+};
 </script>
