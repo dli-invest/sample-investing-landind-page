ec454d12d51e7fede5f216fa3ac583c979ba7076
David Li <davidli012345@gmail.com>
2020-10-11 00:05:36 +0000

    adding services file

---
+++ src/components/Services.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="p-grid">
+  <div class="p-col-12 p-md-6 p-lg-6 p-jc-center">
+    <div class="p-d-flex">
+      <div class="p-d-flex p-flex-column">
+        <div class="p-mb-2">Item 1</div>
+        <div class="p-mb-2">Item 2</div>
+      </div>
+      <div class="p-d-flex p-flex-column">
+        <div class="p-mb-2">Item 1</div>
+        <div class="p-mb-2">Item 2</div>
+      </div>
+    </div>
+  </div>
+  <div class="p-col-12 p-md-6 p-lg-6 p-jc-center"></div>
+</div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,24 @@
 <template>
 <TitleHeader />
+<Services />
 </template>
 
 <script lang="ts">
 import {
-    Options,
-    Vue
+  Options,
+  Vue
 } from "vue-class-component";
 import Navbar from "@/components/Navbar.vue";
 import Counter from "@/components/Counter.vue";
-import TitleHeader from "@/components/TitleHeader.vue"
+import TitleHeader from "@/components/TitleHeader.vue";
+import Services from "@/components/Services.vue";
 @Options({
-    components: {
-        Navbar,
-        Counter,
-        TitleHeader
-    },
+  components: {
+    Navbar,
+    Counter,
+    TitleHeader,
+    Services,
+  },
 })
 export default class Home extends Vue {}
 </script>
