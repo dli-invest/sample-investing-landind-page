28a4f484ba78d5527f378113c6ff3200d1084e13
dli <davidli012345@gmail.com>
2020-11-01 21:20:48 -0800

    adding more examples

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,43 @@
 <template>
-<TitleHeader />
-<div class="custom-container">
+  <TitleHeader />
+  <div class="custom-container">
     <Projects />
-</div>
-<div class="custom-container">
+  </div>
+  <div class="custom-container">
     <Services />
-</div>
-<div class="custom-container">
+  </div>
+  <div class="custom-container">
     <Contact />
-</div>
+  </div>
+  <div class="custom-container">
+    <Examples />
+  </div>
 </template>
 
 <style scoped>
 .custom-container {
-    margin-top: 2em;
-    margin-bottom: 2em;
+  margin-top: 2em;
+  margin-bottom: 2em;
 }
 </style>
 
 <script lang="ts">
-import {
-    Options,
-    Vue
-} from "vue-class-component";
+import { Options, Vue } from "vue-class-component";
 import Navbar from "@/components/Navbar.vue";
 import TitleHeader from "@/components/TitleHeader.vue";
 import Services from "@/components/Services.vue";
 import Projects from "@/components/Projects.vue";
-import Contact from '@/components/Contact.vue'
+import Contact from "@/components/Contact.vue";
+import Examples from "@/components/Examples.vue";
 @Options({
-    components: {
-        Navbar,
-        Contact,
-        TitleHeader,
-        Services,
-        Projects
-    },
+  components: {
+    Navbar,
+    Contact,
+    TitleHeader,
+    Services,
+    Projects,
+    Examples,
+  },
 })
 export default class Home extends Vue {}
 </script>
