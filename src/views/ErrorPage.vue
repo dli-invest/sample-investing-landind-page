b1ab48b1ef8207ad1e8535fc24dcd83c974a610c
dli <davidli012345@gmail.com>
2020-11-02 20:47:46 -0800

    creating a fallback page

---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <Navbar />
+    <Counter />
+    <h1>Failure Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+@Options({
+  components: {
+    HelloWorld,
+    Navbar,
+  },
+})
+export default class Home extends Vue {}
+</script>
